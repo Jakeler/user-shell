3ffe7ea0a196e60d0178383ac7f67bed0c70dcdf
Jake <git@ja-ke.tech>
2018-06-06 16:36:07 +0200

    Add console input method

--- src/main.c
+++ src/main.c
@@ -2,6 +2,21 @@
 
 #include <stdio.h>
 
+#define INPUT_BUFFER_SIZE 3
+
+void readInput() {
+    char x[INPUT_BUFFER_SIZE];
+    // Error on buffer overflow
+    char* ret = fgets(x, INPUT_BUFFER_SIZE, stdin);
+    if(ret == NULL) {
+        fprintf(stderr, "Cannot read input\n");
+    }
+//    printf("Input: %s\n", x);
+//    printf("Return: %s\n", ret);
+//    printf("end: %d\n", x[INPUT_BUFFER_SIZE-1]);
+    
+}
+
 int main() {
     config_file_t *cf = read_config_file("./config.conf");
     if (cf == NULL) {
@@ -18,5 +33,12 @@ int main() {
     if(!release_config(cf)) {
         fprintf(stderr, "Error: Could not free config\n");
     }
+    
+    
+    
+    readInput();
+    readInput();
+
+    
     return 0;
 }
