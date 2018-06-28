05bdb0f6847f5e58c2dc87478e411790244a8387
Jake <git@ja-ke.tech>
2018-06-29 01:39:43 +0200

    Fix install path

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 install -Dm 755 build/src/ush /usr/bin/ush
-setcap cap_setuid,cap_setgid+ep ush
+setcap cap_setuid,cap_setgid+ep /usr/bin/ush
 install -dm 1777 /etc/ush
 
 useradd jk -G users,adm
